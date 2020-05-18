2131b2ca838eaea2d09fa4f9eaeaa02b87956561
Juan Pablo Itto <jitto@tiempodevelopment.com>
2020-05-17 18:06:09 -0700

    adding server for heroku prod

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+  });
+}
+app.listen(process.env.PORT || 5800);
