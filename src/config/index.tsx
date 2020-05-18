7410f97f7c30b076af42de1481db81ee6d4b1f32
Juan Pablo Itto <jitto@tiempodevelopment.com>
2020-05-17 19:21:20 -0700

    Return just the config object

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,16 +1,4 @@
-const env = process.env.NODE_ENV;
-
-let config = null;
-
-try {
-	config = require(`./env/${env}`);
-} catch (ex) {}
-
-export default Object.assign(
-	{},
-	{
-		GOOGLE_API_URL: process.env.REACT_APP_GOOGLE_API_URL,
-		GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY
-	},
-	config || {}
-);
+export default {
+  GOOGLE_API_URL: process.env.REACT_APP_GOOGLE_API_URL,
+  GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY
+};
